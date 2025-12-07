5a0ebbfc0a94995b1ab1bb13b5346b9f725c5de5
GauravTayadeDevloper <gauravtayade619@gmail.com>
2025-12-07 15:43:46 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
-
+import ChildComponent from "./ChildComponent";
 const App = () => {
+ let [todos,settodos] = useState([    { id: 1, text: "Learn React", completed: false },
+    { id: 2, text: "Build a React app", completed: false },
+    { id: 3, text: "Deploy the React app", completed: false }])
+    let handleComplete = (id)=>{
+      settodos(prev=>prev.map(todo=>todo.id===id ? {...todo, completed:true}:todo))
+    }
+
   return (
     <div>
+      <h1>Parent Component</h1>
         {/* Do not remove the main div */}
+        <ChildComponent todos={todos} handleComplete={handleComplete}/>       
     </div>
   )
 }
---
+++ src/components/ChildComponent.js
@@ -0,0 +1,13 @@
+import React from "react"
+export default function ChildComponent({ todos, handleComplete }) {
+    return (
+        <div>
+            <h2>Child Component</h2>
+            <ul>
+                {todos.map((l) => (
+                    <li key={l.id}>{l.text} {!l.completed && (<button onClick={() => handleComplete(l.id)}>Complete</button>)}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
